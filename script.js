3231a16618cf971c899cdfd09be78ea2f9494343
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-10 15:24:05 +0100

    cambiamento html e funzione jvs

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const button = document.querySelector('.button');
+
+const lampContainer= document.getElementById ('lampcontainer')
+
+
  button.addEventListener ('click', switchOnFunction)
 
 
 //  FUNZIONE
 
 function switchOnFunction() {
-
     
-    
+ lampContainer.classList.remove("lampcontaineron");
+ 
     
 }
